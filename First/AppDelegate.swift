e472ac8ccc514eb64f242f6f1f77a44da3d33db7
Pedro Borges <pb106@fs-hp1dhtstg8wa.local>
2016-12-30 00:07:45 -0500

    Initial Commit

---
+++ First/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  First
+//
+//  Created by Pedro Borges on 12/30/16.
+//  Copyright © 2016 Pedro Borges. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ First/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Pedro Borges on 12/30/16.
+//  Copyright © 2016 Pedro Borges. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
